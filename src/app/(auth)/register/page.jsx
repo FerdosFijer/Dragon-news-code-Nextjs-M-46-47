fc12923d0d03e25b6d08e361b1f107c3f309d575
FerdosFijer <ffijer@gmail.com>
2026-07-26 21:32:26 +0600

    different route ready

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h2>Sign up for an account</h2>
+        </div>
+    );
+};
+
+export default RegisterPage;
---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            <h2>News all details are here</h2>
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
